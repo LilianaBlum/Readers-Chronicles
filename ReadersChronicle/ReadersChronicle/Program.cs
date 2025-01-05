0d911e2b1a2736feb5a5804e4a056a4f92a51cfc
Liliana Blumberga <liliana.blumberga@inbox.lv>
2025-01-05 15:10:06 +0200

    dockerfile fix

--- ReadersChronicle/ReadersChronicle/Program.cs
+++ ReadersChronicle/ReadersChronicle/Program.cs
@@ -73,7 +73,7 @@ namespace ReadersChronicle
                 app.UseHsts();
             }
             
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
